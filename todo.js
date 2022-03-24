a5d2746aeeacb00a0a3b4e6babecb593a90577a5
Mateus Proto <66911264+mateusproto@users.noreply.github.com>
2022-03-23 23:51:01 -0300

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,7 +1,13 @@
+import garbage from './garbage-can.png';
+
 function Todo({todo,index,remove}){
   function handle(){
     console.log('Ping:',index);
     remove(index);
   }
-  return <div className="todo" onClick={handle}>{todo.text}><img className="trash"></div>
+  return (
+    <div className="todo" onClick={handle}>{todo.text}>
+      <img className="trash" src={garbage}>
+    </div>
+  );
 }
